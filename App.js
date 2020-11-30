0914d86e97c489a7537419131bbf2dc58b373ce4
Nishit_Aggarwal <63299443+nishitaggarwal@users.noreply.github.com>
2020-11-30 18:00:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createAppContainer ,createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import ExchangeScreen from './screens/ExchangeScreen';
+import HomeScreen from './screens/HomeScreen';
+import WelcomeScreen from './screens/WelcomeScreen'
+
+export default class App extends React.Component {
+  render(){
+    return (
+
+        <AppContainer/>
+
+    );
+  }
+}
+
+
+
+const TabNavigator = createBottomTabNavigator({
+  Exchange: {screen: ExchangeScreen},
+  Home: {screen: HomeScreen},
+}
+)
+
+
+const switchNavigator = createSwitchNavigator({
+WelcomeScreen:{screen: WelcomeScreen},
+TabNavigator:{screen: TabNavigator}
+})
+
+const AppContainer =  createAppContainer(switchNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
---
+++ components/AppTabNavigator.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+import { Image } from 'react-native';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import ExchangeScreen from '../screens/ExchangeScreen'
+import HomeScreen from '../screens/HomeScreen'
+
+export const AppTabNavigator = createBottomTabNavigator({
+ 
+    ItemRequest:{
+        screen:ExchangeScreen,
+        navigationOptions:{
+            tabBarLabel:"Item Request"
+        }
+    },
+})
